43d2750669a352d9476a07b6ca809e0f47c72259
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-30 18:30:28 +0000

    Added DBContext class

---
+++ Enitiy framework with Swagger API/Enitiy framework with Swagger API/Context/CRUDContext.cs
@@ -0,0 +1,29 @@
+﻿using Enitiy_framework_with_Swagger_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enitiy_framework_with_Swagger_API.Context
+{
+    public class CRUDContext : DbContext
+    {
+
+
+        public CRUDContext(DbContextOptions<CRUDContext> options) : base(options)
+        { 
+        
+        
+
+
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+
+
+
+
+    }
+}
