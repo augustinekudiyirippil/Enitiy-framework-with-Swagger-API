9c6e2815ac5ba4e4a1cce83f5ce3e4b6989fb1e6
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-30 16:44:48 +0000

    Project files created

---
+++ Enitiy framework with Swagger API/Enitiy framework with Swagger API/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Enitiy_framework_with_Swagger_API.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
