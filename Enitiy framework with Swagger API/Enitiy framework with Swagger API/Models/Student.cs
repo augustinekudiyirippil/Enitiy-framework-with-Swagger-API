861b617c71891530c69b982816b1ef08038cf61b
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-30 17:20:55 +0000

    Added student model class

---
+++ Enitiy framework with Swagger API/Enitiy framework with Swagger API/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Enitiy_framework_with_Swagger_API.Models
+{
+
+
+    public class Student
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int studentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Roll { get; set; }
+    }
+}
