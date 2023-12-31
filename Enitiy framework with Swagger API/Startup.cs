698eb435884aa1fd5c26f428522111763bc014ff
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-31 10:34:35 +0000

    Swagger and DBCOntect service added in Startup class

--- Enitiy framework with Swagger API/Startup.cs
+++ Enitiy framework with Swagger API/Startup.cs
@@ -1,8 +1,11 @@
+using Enitiy_framework_with_Swagger_API.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +25,31 @@ namespace Enitiy_framework_with_Swagger_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+
+
+            // BELOW CODDED ADDED
+
+            services.AddMvc();
+
+
+            services.AddDbContext<CRUDContext>(options => options.UseSqlServer(Configuration.GetConnectionString("dbUniversity")));
+
+
+            services.AddSwaggerGen(
+                c => c.SwaggerDoc
+                    (
+                    "V1", new OpenApiInfo
+                    {
+                        Title = "My test API",
+                        Version = "V1"
+                    }
+                    )
+                );
+
+            // ABOVE CODE ADDED
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,11 +58,26 @@ namespace Enitiy_framework_with_Swagger_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
             else
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            //BELOW CODE ADDED
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                    }
+             );
+
+
+
+            //ABOVE CODE ADDED
 
             app.UseStaticFiles();
 
