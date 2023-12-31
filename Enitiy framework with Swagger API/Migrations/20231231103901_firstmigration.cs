8c58c5310216803c2af63203ec6cff9f1ba6ac89
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-31 10:47:08 +0000

    Added migration and updated the database

---
+++ Enitiy framework with Swagger API/Migrations/20231231103901_firstmigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Enitiy_framework_with_Swagger_API.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    studentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    studentName = table.Column<string>(nullable: true),
+                    studentRoll = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.studentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
