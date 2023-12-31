44287e8c37ee01dc9165ec6f2c92519764815416
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-31 13:22:26 +0000

    CRUD methods added in the new Student controller

---
+++ Enitiy framework with Swagger API/Controllers/StudentController.cs
@@ -0,0 +1,88 @@
+﻿using Enitiy_framework_with_Swagger_API.Context;
+using Enitiy_framework_with_Swagger_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Enitiy_framework_with_Swagger_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        private readonly CRUDContext  _CRUDContext;
+
+
+        public StudentController(CRUDContext CRUDContext)
+        {
+            _CRUDContext = CRUDContext;
+        }
+
+
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return _CRUDContext.students;
+
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return _CRUDContext.students.SingleOrDefault(x=> x.studentID==id);
+
+        }
+
+
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] Student student)
+        {
+
+            _CRUDContext.students.Add(student);
+            _CRUDContext.SaveChanges();
+
+
+
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put( [FromBody] Student student)
+        {
+            _CRUDContext.students.Add(student);
+            _CRUDContext.SaveChanges();
+
+
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+           
+            var studentToDelete = _CRUDContext.students.SingleOrDefault(x => x.studentID == id);
+            if (studentToDelete != null)
+            {
+
+
+
+                _CRUDContext.students.Remove(studentToDelete);
+                _CRUDContext.SaveChanges();
+
+            }
+
+
+        }
+    }
+}
